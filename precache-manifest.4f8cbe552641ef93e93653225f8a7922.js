379211d757a681f418d1e087364dbc80964259b9
walpolsh <twentytwentyk@live.com>
2020-02-29 11:37:07 -0500

    Updates

---
+++ precache-manifest.4f8cbe552641ef93e93653225f8a7922.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98b5d3ab6e15d7da11082468bda8cd52",
+    "url": "/forte/index.html"
+  },
+  {
+    "revision": "9c04eed4f6c24c63a08b",
+    "url": "/forte/static/css/main.94558d28.chunk.css"
+  },
+  {
+    "revision": "5de23c6b767753e9210a",
+    "url": "/forte/static/js/2.904b3598.chunk.js"
+  },
+  {
+    "revision": "2cf707b79c11642227a9",
+    "url": "/forte/static/js/3.ea827e28.chunk.js"
+  },
+  {
+    "revision": "9c04eed4f6c24c63a08b",
+    "url": "/forte/static/js/main.0aca11f9.chunk.js"
+  },
+  {
+    "revision": "66a4e5e3b26818cccd8e",
+    "url": "/forte/static/js/runtime-main.23e0ca32.js"
+  },
+  {
+    "revision": "d81bf0bc57e8bb613a0e7b4259274fd2",
+    "url": "/forte/static/media/forté-rect.d81bf0bc.png"
+  }
+]);
